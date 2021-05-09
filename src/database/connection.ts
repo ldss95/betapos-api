0f3e9f52c653f67dfe6f79eda1273f8fcf870d2e
Luis David Santiago Santana <luisdavidsantiagosantana@gmail.com>
2021-05-09 10:53:06 -0400

    feat: archivo de conexion con la base de datos

---
+++ src/database/connection.ts
@@ -0,0 +1,26 @@
+import { Sequelize } from 'sequelize'
+
+const { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } = process.env
+
+const db = new Sequelize({
+	dialect: 'mysql',
+	dialectOptions: {
+		dateStrings: true,
+		typeCast: true
+	},
+	define: {
+		charset: 'utf8',
+		collate: 'utf8_general_ci'
+	},
+	timezone: '-04:00',
+	host: DB_HOST,
+	username: DB_USER,
+	password: DB_PASS,
+	port: Number(DB_PORT),
+	database: DB_NAME,
+	logging: false
+})
+
+db.sync()
+
+export { db }
