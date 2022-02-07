050123273a7b57cd445bdcde4cba976aa81faaba
Luis David Santiago <luisdavidsantiagosantana@gmail.com>
2022-02-07 00:59:08 -0400

    feat: controla error de dispositivo duplicado

--- src/components/devices/controller.ts
+++ src/components/devices/controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { UniqueConstraintError } from 'sequelize'
 import { Business } from '../business/model'
 
 import { Os } from '../operative-systems/model'
@@ -19,6 +20,21 @@ export default {
 			await Device.create({ ...req.body, businessId: business.id })
 			res.sendStatus(201)
 		} catch (error) {
+			if (error instanceof UniqueConstraintError) {
+				const device = await Device.findOne({ where: { deviceId: req.body.deviceId } })
+				const { businessId } = device!
+				const business = await Business.findOne({ where: { id: businessId } })
+				const { merchatId } = business!
+
+				if (merchatId == req.body.merchantId) {
+					return res.sendStatus(201)
+				}
+
+				return res.status(400).send({
+					message: 'Esta PC se encuentra conectada con otra cuenta de negocios.'
+				})
+			}
+
 			res.sendStatus(500)
 			throw error
 		}
