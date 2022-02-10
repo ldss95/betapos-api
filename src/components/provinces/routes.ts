96ac8b4cd7bf78e2fe5630e8e9f73003e9e9e953
Luis David Santiago <luisdavidsantiagosantana@gmail.com>
2022-02-09 23:58:38 -0400

    feat: permite obtener las provincias y los tipos de empresa sin login

--- src/components/provinces/routes.ts
+++ src/components/provinces/routes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express'
 const routes: Router = Router()
 
-import { isLoggedin, tokenIsValid } from '../../middlewares/auth'
 import controller from './controller'
 
-routes.get('/', isLoggedin, tokenIsValid, controller.getAll)
+routes.get('/', controller.getAll)
 
 export default routes
