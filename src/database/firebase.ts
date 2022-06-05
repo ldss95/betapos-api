bb480269594545f41f7542676ff8451057453355
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-06-05 09:59:17 -0400

    docs: explica por que importa firebase de la manera que lo hace

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -1,3 +1,10 @@
+/**
+ * Se hace el import de esta manera porque jest falla al hacerlo
+ * como indica la documentacion de firebase
+ * import { initializeApp, applicationDefault } from 'firebase/app';
+ * import { getFirestore } from 'firebase/firestore';
+ */
+
 import * as firebase from 'firebase-admin'
 
 firebase.initializeApp({
