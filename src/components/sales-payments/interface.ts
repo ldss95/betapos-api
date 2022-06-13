27004055025880c48eaabb779aa9800c77e13c02
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-06-12 22:36:00 -0400

    feat: elimina la interfaz de estado del pago (solo se guarda realizado)

--- src/components/sales-payments/interface.ts
+++ src/components/sales-payments/interface.ts
@@ -5,15 +5,6 @@ export interface SalePaymentAttr extends Model {
 	saleId: string;
 	typeId: string;
 	amount: number;
-	statusId: string;
-	createdAt: string;
-	updatedAt: string;
-}
-
-export interface SalePaymentStatusAttr extends Model {
-	id: string;
-	name: string;
-	description: string;
 	createdAt: string;
 	updatedAt: string;
 }
