2677fce5550ed1392dbd509ff5e4f286b13fcf2d
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-07-21 09:29:00 -0400

    feat: agrega columna para url del voucher de transferencia

--- src/components/billing/interface.ts
+++ src/components/billing/interface.ts
@@ -4,8 +4,9 @@ export interface BillAttr extends Model {
 	id: string;
 	businessId: string;
 	orderNumber: string;
-	uepaPayOrderNumber: string;
-	uepaPayLink: string;
+	uepaPayOrderNumber?: string;
+	uepaPayLink?: string;
+	transferVoucherUrl?: string;
 	amount: number;
 	description: string;
 	payed: boolean;
