251621b2e15f1a5b81bc019958493b7a06f6c330
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-07-24 16:40:50 -0400

    feat: permite almacenar facturas con productos repetidos (999)

--- src/components/sales-products/model.ts
+++ src/components/sales-products/model.ts
@@ -4,51 +4,40 @@ import { db } from '../../database/connection'
 import { SaleProductAttr } from './interface'
 import { Product } from '../products/model'
 
-const SaleProduct = db.define<SaleProductAttr>(
-	'sales_product',
-	{
-		id: {
-			type: DataTypes.UUID,
-			primaryKey: true,
-			defaultValue: DataTypes.UUIDV4
-		},
-		saleId: {
-			type: DataTypes.UUID,
-			allowNull: false
-		},
-		productId: {
-			type: DataTypes.UUID,
-			allowNull: false
-		},
-		quantity: {
-			type: DataTypes.DOUBLE,
-			allowNull: false,
-			validate: {
-				min: 0.01
-			}
-		},
-		cost: {
-			type: DataTypes.DOUBLE,
-			allowNull: false,
-			defaultValue: 0
-		},
-		price: {
-			type: DataTypes.DOUBLE,
-			allowNull: false,
-			validate: {
-				min: 0.01
-			}
+const SaleProduct = db.define<SaleProductAttr>('sales_product', {
+	id: {
+		type: DataTypes.UUID,
+		primaryKey: true,
+		defaultValue: DataTypes.UUIDV4
+	},
+	saleId: {
+		type: DataTypes.UUID,
+		allowNull: false
+	},
+	productId: {
+		type: DataTypes.UUID,
+		allowNull: false
+	},
+	quantity: {
+		type: DataTypes.DOUBLE,
+		allowNull: false,
+		validate: {
+			min: 0.01
 		}
 	},
-	{
-		indexes: [
-			{
-				unique: true,
-				fields: ['saleId', 'productId']
-			}
-		]
+	cost: {
+		type: DataTypes.DOUBLE,
+		allowNull: false,
+		defaultValue: 0
+	},
+	price: {
+		type: DataTypes.DOUBLE,
+		allowNull: false,
+		validate: {
+			min: 0.01
+		}
 	}
-)
+})
 
 SaleProduct.belongsTo(Product, { foreignKey: 'productId', as: 'product' })
 
