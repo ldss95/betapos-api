912d1bf8b6a17a4509d5ff22c8ab7ea9d8c746ef
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-08-20 10:35:46 -0400

    feat: crea endpoint para guardar las entradas y salidas de caja

--- src/components/cash-flow/controller.ts
+++ src/components/cash-flow/controller.ts
@@ -1,8 +1,36 @@
 import { Request, Response } from 'express'
 
+import { User } from '../users/model'
 import { CashFlow } from './model'
 
 export default {
+	create: async (req: Request, res: Response) => {
+		try {
+			const { cashFlow } = req.body
+
+			if (!cashFlow) {
+				return res.status(400).send({
+					message: 'Missing data'
+				})
+			}
+
+			const user = await User.findByPk(cashFlow.userId)
+			if (!user) {
+				throw new Error('Usuario no encontrado')
+			}
+
+			const { businessId } = user
+
+			await CashFlow.create({
+				...cashFlow,
+				businessId
+			})
+			res.sendStatus(204)
+		} catch (error) {
+			res.sendStatus(500)
+			throw error
+		}
+	},
 	getAll: (req: Request, res: Response) => {
 		const { businessId } = req.session!
 		CashFlow.findAll({
--- src/components/cash-flow/routes.ts
+++ src/components/cash-flow/routes.ts
@@ -5,6 +5,6 @@ import { isLoggedin, tokenIsValid } from '../../middlewares/auth'
 
 const router: Router = Router()
 
-router.get('/', isLoggedin, tokenIsValid, controller.getAll)
+router.route('/').get(isLoggedin, tokenIsValid, controller.getAll).post(controller.create)
 
 export default router
