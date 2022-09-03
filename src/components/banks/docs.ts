beeccf423a4a922d45264e5678170c48197fa827
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-09-03 18:18:51 -0400

    docs: crea documentacion para endpoints de bancos

---
+++ src/components/banks/docs.ts
@@ -0,0 +1,48 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Bancos
+ *  description: Endpoints de bancos
+ */
+
+/**
+ * @swagger
+ * /banks:
+ *  get:
+ *      tags: [Bancos]
+ *      summary: Obtiene la lista de bancos
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: string
+ *                                      example: 16c3a5b7-8ff3-489d-ae90-c66de7610db3
+ *                                  name:
+ *                                      type: string
+ *                                      example: Banco Popular Dominicano
+ *                                  createdAt:
+ *                                      type: string
+ *                                      example: 2022-01-01 08:00:00
+ *                                  updatedAt:
+ *                                      type: string
+ *                                      example: 2022-01-01 08:00:00
+ *          401:
+ *              description: No autenticado
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  description: Mensaje explicando que pasó
+ *                                  example: Ninguna sesion iniciada
+ *
+ */
