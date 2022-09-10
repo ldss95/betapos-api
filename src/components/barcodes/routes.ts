97850e2531eafb7595fff2fba4fc645b1a514fe7
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-09-10 12:16:46 -0400

    feat: agrega middleware de validacion a endpoint de barcodes

--- src/components/barcodes/routes.ts
+++ src/components/barcodes/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import { hasMerchantId } from '../../middlewares/auth'
 const router: Router = Router()
 
 import controller from './controller'
 
-router.get('/updates/:date', controller.getUpdates)
+router.get('/updates/:date', hasMerchantId, controller.getUpdates)
 
 export default router
