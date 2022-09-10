ad4478aeeb1be535309646bfc8b1a520fab11619
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-09-10 11:58:42 -0400

    feat: restringe endpoint para marcar facturas como pagas a administradores

--- src/components/billing/routes.ts
+++ src/components/billing/routes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express'
 
 const routes: Router = Router()
 import controller from './controller'
-import { isLoggedin } from '../../middlewares/auth'
+import { isLoggedin, isAdmin } from '../../middlewares/auth'
 import { uploadSingle } from '../../middlewares/files'
 
 routes.route('/').get(isLoggedin, controller.getAll)
 
-routes.post('/pay', isLoggedin, uploadSingle('payments/'), controller.markAsPayed)
+routes.post('/pay', isLoggedin, isAdmin, uploadSingle('payments/'), controller.markAsPayed)
 
 export default routes
