2014b0277d26e9cd036857bc443fc82ffd0f4c7b
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-03 06:48:18 -0400

    feat: agrega validacion para checar si el header deviceId existe

--- src/components/shifts/routes.ts
+++ src/components/shifts/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 const router: Router = Router()
 
-import { isLoggedin, tokenIsValid } from '../../middlewares/auth'
+import { hasDeviceId, isLoggedin, tokenIsValid } from '../../middlewares/auth'
 import controller from './controller'
 
-router.route('/').post(controller.create).put(controller.update).get(isLoggedin, tokenIsValid, controller.getAll)
+router.route('/')
+	.post(hasDeviceId, controller.create)
+	.put(hasDeviceId, controller.update)
+	.get(isLoggedin, tokenIsValid, controller.getAll)
 
 router.get('/sold-details/:shiftId', isLoggedin, tokenIsValid, controller.getSoldDetails)
 
