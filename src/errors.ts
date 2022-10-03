557bd0ac7c26817985a4c459fe8771a5822f8251
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-03 06:55:10 -0400

    feat: permite enviar el status del error en CustomError

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,9 @@
 export class CustomError {
 	public readonly message: string
+	public readonly status: number
 
-	constructor({ message }: { message: string }) {
+	constructor({ message, status }: { message: string; status?: number }) {
 		this.message = message
+		this.status = status ?? 400
 	}
 }
