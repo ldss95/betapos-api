f8f1f1817e64232702ed45385f27a3f46b6a78df
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-10-03 09:11:36 -0400

    refactor: mueve logica de turnos a servicios

--- src/components/shifts/controller.ts
+++ src/components/shifts/controller.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express'
-import { literal, Op, fn, col } from 'sequelize'
 
-import { Shift } from './model'
-import { User } from '../users/model'
-import { Sale } from '../sales/model'
-import { SalePaymentType } from '../sales-payments-types/model'
-import { createShift, updateShift } from './services'
+import { createShift, getAllShifts, getShiftSummary, updateShift } from './services'
 import { CustomError } from '../../errors'
 
 export default {
@@ -46,104 +41,23 @@ export default {
 	getAll: async (req: Request, res: Response) => {
 		try {
 			const { businessId } = req.session!
-			const { date, userId } = req.query
+			const { date, userId } = req.query as { [key: string]: string }
 
-			const shifts = await Shift.findAll({
-				attributes: {
-					include: [
-						[
-							literal('(SELECT SUM(amount) FROM sales WHERE shiftId = shift.id AND status = \'DONE\')'),
-							'totalSold'
-						],
-						[
-							literal(`
-								(
-									SELECT
-										SUM(sp.amount)
-									FROM
-										sales s
-									LEFT JOIN
-										sales_payments sp ON sp.saleId = s.id
-									LEFT JOIN
-										sales_payment_types spt ON spt.id = sp.typeId
-									WHERE
-										shiftId = shift.id AND
-										status = 'DONE' AND
-										spt.name = 'Efectivo'
-								)
-							`),
-							'totalSoldCash'
-						],
-						[
-							literal(`
-								(
-									COALESCE((
-										SELECT
-											SUM(-1 * amount)
-										FROM
-											cash_flows
-										WHERE
-											shiftId = shift.id AND
-											type = 'OUT'
-									), 0) +
-									COALESCE((
-										SELECT
-											SUM(amount)
-										FROM
-											cash_flows
-										WHERE
-											shiftId = shift.id AND
-											type = 'IN'
-									), 0)
-								)
-							`),
-							'cashFlow'
-						]
-					]
-				},
-				include: {
-					model: User,
-					as: 'user',
-					where: {
-						businessId
-					}
-				},
-				order: [['date', 'DESC']],
-				where: {
-					[Op.and]: [
-						{
-							...(date && { date })
-						},
-						{
-							...(userId && { userId })
-						}
-					]
-				}
-			})
+			const shifts = await getAllShifts(businessId, date, userId)
 			res.status(200).send(shifts)
 		} catch (error) {
 			res.sendStatus(500)
 			throw error
 		}
 	},
-	getSoldDetails: (req: Request, res: Response) => {
-		const { shiftId } = req.params
-
-		Sale.findAll({
-			attributes: [[fn('sum', col('amount')), 'total']],
-			where: {
-				shiftId
-			},
-			include: {
-				model: SalePaymentType,
-				as: 'paymentType'
-			},
-			group: 'paymentTypeId'
-		})
-			.then((results) => res.status(200).send(results))
-			.catch((error) => {
-				res.sendStatus(500)
-				throw error
-			})
+	getSoldDetails: async (req: Request, res: Response) => {
+		try {
+			const { shiftId } = req.params
+			const summary = await getShiftSummary(shiftId)
+			res.status(200).send(summary)
+		} catch (error) {
+			res.sendStatus(500)
+			throw error
+		}
 	}
 }
--- src/components/shifts/services.ts
+++ src/components/shifts/services.ts
@@ -1,5 +1,11 @@
+import { literal, Op, col, fn } from 'sequelize'
+
 import { CustomError } from '../../errors'
 import { Device } from '../devices/model'
+import { SalePaymentTypeProps } from '../sales-payments-types/interface'
+import { SalePaymentType } from '../sales-payments-types/model'
+import { Sale } from '../sales/model'
+import { User } from '../users/model'
 import { ShiftProps } from './interface'
 import { Shift } from './model'
 
@@ -33,4 +39,101 @@ export async function updateShift(shiftId: string, deviceId: string, data: Shift
 	}
 
 	await shift.update(data)
+}
+
+export async function getAllShifts(businessId: string, date?: string, userId?: string): Promise<ShiftProps[]> {
+	const shifts = await Shift.findAll({
+		attributes: {
+			include: [
+				[
+					literal('(SELECT SUM(amount) FROM sales WHERE shiftId = shift.id AND status = \'DONE\')'),
+					'totalSold'
+				],
+				[
+					literal(`
+						(
+							SELECT
+								SUM(sp.amount)
+							FROM
+								sales s
+							LEFT JOIN
+								sales_payments sp ON sp.saleId = s.id
+							LEFT JOIN
+								sales_payment_types spt ON spt.id = sp.typeId
+							WHERE
+								shiftId = shift.id AND
+								status = 'DONE' AND
+								spt.name = 'Efectivo'
+						)
+					`),
+					'totalSoldCash'
+				],
+				[
+					literal(`
+						(
+							COALESCE((
+								SELECT
+									SUM(-1 * amount)
+								FROM
+									cash_flows
+								WHERE
+									shiftId = shift.id AND
+									type = 'OUT'
+							), 0) +
+							COALESCE((
+								SELECT
+									SUM(amount)
+								FROM
+									cash_flows
+								WHERE
+									shiftId = shift.id AND
+									type = 'IN'
+							), 0)
+						)
+					`),
+					'cashFlow'
+				]
+			]
+		},
+		include: {
+			model: User,
+			as: 'user',
+			where: {
+				businessId
+			}
+		},
+		order: [['date', 'DESC']],
+		where: {
+			[Op.and]: [
+				{
+					...(date && { date })
+				},
+				{
+					...(userId && { userId })
+				}
+			]
+		}
+	})
+
+	return shifts
+}
+
+interface ShiftSummaryProps {
+	total: number;
+	paymentType: SalePaymentTypeProps
+}
+export async function getShiftSummary(shiftId: string): Promise<ShiftSummaryProps[]> {
+	const items = await Sale.findAll({
+		attributes: [[fn('sum', col('amount')), 'total']],
+		where: {
+			shiftId
+		},
+		include: {
+			model: SalePaymentType,
+			as: 'paymentType'
+		},
+		group: 'paymentTypeId'
+	})
+
+	return items.map(item => item.toJSON()) as ShiftSummaryProps[]
 }
