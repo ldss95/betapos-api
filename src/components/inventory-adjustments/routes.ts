ee3fa42acb78201cdeb36ed1ca1b0c04984168be
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2022-11-11 23:28:38 -0400

    feat: hace que el ajuste de inventario sea incremental
    Solo aplica para Supermercado Libertad

--- src/components/inventory-adjustments/routes.ts
+++ src/components/inventory-adjustments/routes.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 
 import controller from './controller'
 import { isLoggedIn, tokenIsValid } from '../../middlewares/auth'
+import { inventoryAdjustmentInterceptor } from './middlewares'
 const routes: Router = Router()
 
-routes.post('/', isLoggedIn, tokenIsValid, controller.create)
+routes.post('/', isLoggedIn, tokenIsValid, inventoryAdjustmentInterceptor, controller.create)
 
 export default routes
