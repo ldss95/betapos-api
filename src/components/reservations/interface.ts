626289b11cd7c089255574ba6b057d09628ca159
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2023-04-01 20:26:40 -0400

    feat: interface para reservas

---
+++ src/components/reservations/interface.ts
@@ -0,0 +1,9 @@
+export interface ReservationProps {
+	id: string;
+	clientName: string;
+	diners: number; // Cantidad de personas
+	date: string;
+	time: string;
+	createdAt: string;
+	updatedAt: string;
+}
