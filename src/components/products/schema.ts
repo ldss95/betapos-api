d3d1a8995ea82c6a723dafa7ec030323958ab63a
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2023-04-17 15:29:15 -0400

    fix: evita error si se envia un string vacio como referencia

--- src/components/products/schema.ts
+++ src/components/products/schema.ts
@@ -9,7 +9,17 @@ export const ProductSchema = z.object({
 	name: z.string().min(1),
 	categoryId: z.string().uuid().nullable().optional(),
 	barcodes: z.array(BarcodeSchema).optional(),
-	referenceCode: z.string().min(3).nullable().optional(),
+	referenceCode: z
+		.string()
+		.nullable()
+		.optional()
+		.refine((val) => {
+			if (!val) {
+				return true
+			}
+
+			return val?.replace(/ /g, '') === '' || (val?.length || 0) >= 3
+		}, { message: 'Código de referencia muy corto' }),
 	initialStock: z.number().optional(),
 	cost: z.number().nullable().optional(),
 	price: z.number().nullable().optional(),
