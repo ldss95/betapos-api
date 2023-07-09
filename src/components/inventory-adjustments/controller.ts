2657e4663413a0990ffde70a5a3b6ef73bc0013a
Luis Santiago <luisdavidsantiagosantana@gmail.com>
2023-07-08 21:20:44 -0400

    feat: ignora reset

--- src/components/inventory-adjustments/controller.ts
+++ src/components/inventory-adjustments/controller.ts
@@ -1,37 +1,11 @@
 import { Request, Response } from 'express'
-import { Op } from 'sequelize'
 
-import { InventoryAdjustment } from './model'
 import { createInventoryAdjustment } from './services'
-import moment from 'moment'
 
 export default {
 	create: async (req: Request, res: Response) => {
 		const { userId } = req.session!
 		const { productId, quantity, description } = req.body
-
-		const todayR = await InventoryAdjustment.count({
-			where: {
-				[Op.and]: [
-					{ productId },
-					{
-						createdAt: {
-							[Op.gt]: moment().format('YYYY-MM-DD 00:00:00')
-						}
-					}
-				]
-			}
-		})
-
-		if (todayR === 0) {
-			await createInventoryAdjustment({
-				type: 'X',
-				productId,
-				quantity: 0,
-				userId,
-				description
-			})
-		}
 
 		await createInventoryAdjustment({
 			userId,
