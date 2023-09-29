d28d5346ee7c8c38b83e1a92c669ac37412a0f50
Luis Santiago <lsantiago@pixnabilab.com>
2023-09-28 22:34:20 -0400

    refactor: elimina rastro de whatsapp

--- src/components/ws/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-const router: Router = Router()
-
-import { isBusinessAdmin, isLoggedIn, tokenIsValid } from '../../middlewares/auth'
-import controller from './controller'
-
-router.get('/qr', isLoggedIn, tokenIsValid, isBusinessAdmin, controller.getQr)
-
-export default router
--- src/routes.ts
+++ src/routes.ts
@@ -34,7 +34,6 @@ import computingScales from './components/computing-scales/routes'
 import plans from './components/plans/routes'
 import inventory from './components/inventory/routes'
 import leads from './components/leads/routes'
-import ws from './components/ws/routes'
 
 router.use('/auth', auth)
 router.use('/users', users)
@@ -67,7 +66,6 @@ router.use('/purchases', purchases)
 router.use('/computing-scales', computingScales)
 router.use('/inventory', inventory)
 router.use('/leads', leads)
-router.use('/ws', ws)
 
 router.all('*', (req, res) => res.status(404).sendFile(join(__dirname, '404.html')))
 
